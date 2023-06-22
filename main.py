09092ad38f391ac925a07ab7a206774891c2f2b8
Elfat M <104862141+elfat-py@users.noreply.github.com>
2023-06-22 10:03:15 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,337 @@
+
+import turtle
+import random
+
+turtle.title("I AM SORRY :(")
+WIDTH = 900
+HEIGHT = 600
+turtle.screensize(WIDTH, HEIGHT)
+
+
+
+# Create a turtle object
+# k = turtle.Turtle()
+#
+# # Set the pen color and size
+# k.pencolor('#ff3f34')
+# k.pensize(5)
+#
+# # Move the turtle into position
+# k.penup()
+# k.goto(-400, 100)
+# k.pendown()
+#
+# # Draw the letter 'K'
+# k.left(90)
+# k.forward(100)
+# k.backward(50)
+# k.right(45)
+# k.forward(70)
+# k.backward(70)
+# k.right(90)
+# k.forward(70)
+# k.backward(70)
+# k.hideturtle()
+#
+# #L
+# l = turtle.Turtle()
+# l.pencolor('#ff3f34')
+# l.pensize(5)
+# l.penup()
+# l.goto(-300, 100)
+# l.pendown()
+# l.left(90)
+# l.forward(100)
+# l.hideturtle()
+#
+# u = turtle.Turtle()
+# u.pencolor('#ff3f34')
+# u.pensize(5)
+# u.penup()
+# u.goto(-200, 170)
+# u.left(180)
+# u.pendown()
+# u.left(90)
+# u.forward(40)
+# u.circle(-30, 180)
+# u.forward(40)
+# u.hideturtle()
+#
+#
+# p = turtle.Turtle()
+# p.pencolor('#ff3f34')
+# p.pensize(5)
+# p.penup()
+# p.goto(-160, 70)
+# p.pendown()
+# p.left(90)
+# p.forward(100)
+# p.right(90)
+# p.circle(-30, 180)
+# p.right(90)
+# p.forward(-70)
+# p.hideturtle()
+#
+# i = turtle.Turtle()
+# i.pencolor('#ff3f34')
+# i.pensize(5)
+# i.penup()
+# i.goto(-80, 100)
+# i.pendown()
+# i.left(90)
+# i.forward(70)
+# i.penup()
+# i.backward(50)
+# i.pendown()
+# i.forward(10)
+# i.hideturtle()
+
+# i1 = turtle.Turtle()
+# i1.pencolor('#3f37c9')
+# i1.pensize(5)
+# i1.penup()
+# i1.position()
+# i1.setx(-400)
+# i1.sety(-100)
+# i1.pendown()
+# i1.forward(10)
+# i1.backward(20)
+# i1.forward(10)
+# i1.left(90)
+# i1.forward(100)
+# i1.right(90)
+# i1.forward(10)
+# i1.backward(20)
+# i1.forward(10)
+# i1.hideturtle()
+
+
+n = turtle.Turtle()
+n.pencolor('#ff3f34')
+n.pensize(5)
+n.penup()
+n.goto(-330, -100)
+n.left(360)
+n.pendown()
+n.left(90)
+n.forward(40)
+n.circle(-30, 180)
+n.forward(40)
+n.hideturtle()
+
+o = turtle.Turtle()
+o.pencolor('#4895ef')
+o.pensize(5)
+o.resizemode("user")
+o.shapesize(0.4, 0.4, 0.4)
+o.shapesize()
+o.shape("turtle")
+o.speed(10)
+o.penup()
+o.position()
+o.setx(-170)
+o.sety(0)
+o.pendown()
+for x in range(360):
+    o.speed(50)
+    o.backward(0.9)
+    o.left(1)
+o.hideturtle()
+
+
+
+v = turtle.Turtle()
+v.pencolor('#ff3f34')
+v.pensize(5)
+v.penup()
+v.goto(-150, -100)
+v.pendown()
+v.left(60)
+v.forward(100)
+v.right(120)
+v.forward(100)
+
+# Hide the turtle
+v.hideturtle()
+
+
+
+
+
+turtle.done()
+
+
+# ANTIGONA
+# A
+# a = turtle.Turtle()
+# a.speed(5)
+# a.pencolor('#f72585')
+# a.pensize(5)
+# a.shape("classic")
+# a.penup()
+# a.position()
+# a.setx(-400)
+# a.sety(100)
+# a.pendown()
+# a.hideturtle()
+# a.left(70)
+# a.forward(100)
+# a.right(140)
+# a.forward(100)
+# a.backward(50)
+# a.right(110)
+# a.forward(20)
+# a.hideturtle()
+
+#K
+
+# # N
+# n = turtle.Turtle()
+# n.pencolor('#7209b7')
+# n.pensize(5)
+# n.penup()
+# n.position()
+# n.setx(-300)
+# n.sety(100)
+# n.pendown()
+# u.left(90)
+# u.circle(-50, 180)
+# u.forward(100)
+#
+# u.hideturtle()
+
+
+
+#
+# # T
+# l = turtle.Turtle()
+# l.pencolor('#560bad')
+# l.pensize(5)
+# l.penup()
+# l.position()
+# l.setx(-170)
+# l.sety(100)
+# l.pendown()
+# l.left(90)
+# l.forward(100)
+# l.right(90)
+# l.forward(30)
+# l.backward(60)
+# l.hideturtle()
+#
+# # I
+# i = turtle.Turtle()
+# i.pencolor('#3f37c9')
+# i.pensize(5)
+# i.penup()
+# i.position()
+# i.setx(-100)
+# i.sety(100)
+# i.pendown()
+# i.forward(10)
+# i.backward(20)
+# i.forward(10)
+# i.left(90)
+# i.forward(100)
+# i.right(90)
+# i.forward(10)
+# i.backward(20)
+# i.forward(10)
+# i.hideturtle()
+#
+# # G
+# g = turtle.Turtle()
+# g.pencolor('#4361ee')
+# g.pensize(5)
+# g.resizemode("user")
+# g.shapesize(0.7, 0.7, 0.7)
+# g.shapesize()
+# g.shape("turtle")
+# g.speed(10)
+# g.penup()
+# g.position()
+# g.setx(0)
+# g.sety(200)
+# g.pendown()
+# for x in range(180):
+#     g.speed(10)
+#     g.backward(0.9)
+#     g.left(1)
+# g.right(90)
+# g.forward(50)
+# g.right(90)
+# g.backward(25)
+# g.hideturtle()
+#
+# # O
+# o = turtle.Turtle()
+# o.pencolor('#4895ef')
+# o.pensize(5)
+# o.resizemode("user")
+# o.shapesize(0.7, 0.7, 0.7)
+# o.shapesize()
+# o.shape("turtle")
+# o.speed(10)
+# o.penup()
+# o.position()
+# o.setx(100)
+# o.sety(200)
+# o.pendown()
+# for x in range(360):
+#     o.speed(10)
+#     o.backward(0.9)
+#     o.left(1)
+# o.hideturtle()
+#
+# # Second N
+# n1 = turtle.Turtle()
+# n1.pencolor('#7209b7')
+# n1.pensize(5)
+# n.shape("circle")
+# n1.penup()
+# n1.position()
+# n1.setx(200)
+# n1.sety(100)
+# n1.pendown()
+# n1.left(90)
+# n1.forward(100)
+# n1.right(150)
+# n1.forward(115)
+# n1.left(150)
+# n1.forward(100)
+# n1.hideturtle()
+#
+# # Second A
+# a1 = turtle.Turtle()
+# a1.pencolor('#f72585')
+# a1.pensize(5)
+# a1.shape("classic")
+# a1.penup()
+# a1.position()
+# a1.setx(300)
+# a1.sety(100)
+# a1.pendown()
+# a1.left(70)
+# a1.forward(100)
+# a1.right(140)
+# a1.forward(100)
+# a1.backward(50)
+# a1.right(110)
+# a1.forward(20)
+# a1.hideturtle()
+#
+# # Star
+# spiral = turtle.Turtle()
+# colours = ["red", "blue", "black"]
+# spiral.position()
+# spiral.penup()
+# spiral.setx(100)
+# spiral.sety(15)
+# spiral.pendown()
+# for i in range(40):
+#     spiral.color(random.choice(colours))
+#     spiral.forward(i * 10)
+#     spiral.right(144)
+#
+# turtle.done()
+
